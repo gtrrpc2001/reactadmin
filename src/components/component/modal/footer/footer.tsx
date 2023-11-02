d0516a432e869e3f844b0417a19622bf468e735d
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-11-02 11:44:20 +0900

    Update footer.tsx

--- src/components/component/modal/footer/footer.tsx
+++ src/components/component/modal/footer/footer.tsx
@@ -22,25 +22,25 @@ export const Footer = ({onClick,footerClick}:Props) => {
     
     return (       
         <Box sx={{width:330,height:70,display:'flex',bottom:0,alignItems:'center',":hover":{cursor:'default'}}}>
-            <Box onClick={(e) =>onClick(e)} sx={{width:50,marginLeft:2}}>
+            <Box id="home" onClick={(e) =>onClick(e)} sx={{width:50,marginLeft:2}}>
                 <HouseOutlinedIcon sx={{color:home,marginLeft:1}}  fontSize="large"/>                  
                 <Typography sx={{fontWeight:'bold', color:home,position:'absolute',bottom:buttom,left:33}}>
                     {home == "#ef507b" ? "홈" : ""}
                 </Typography>
             </Box>
-            <Box onClick={(e) =>onClick(e)} sx={{width:60,marginLeft:2}}>
+            <Box id="graph" onClick={(e) =>onClick(e)} sx={{width:60,marginLeft:2}}>
                 <InsertChartOutlinedIcon sx={{color:graph,marginLeft:3}}  fontSize="large"/>                  
                 <Typography sx={{fontWeight:'bold', color:graph,position:'absolute',bottom:buttom,left:107}}>
                     {graph == "#ef507b" ? "요약" : ""}
                 </Typography>
             </Box>
-            <Box onClick={(e) =>onClick(e)} sx={{width:120}}>
+            <Box id="pulse" onClick={(e) =>onClick(e)} sx={{width:120}}>
                 <MonitorHeartOutlinedIcon sx={{color:pulse,marginLeft:7.5}}  fontSize="large"/>                  
                 <Typography sx={{fontWeight:'bold', color:pulse,position:'absolute',bottom:buttom,right:90}}>
                     {pulse == "#ef507b" ? "비정상맥박" : ""}
                 </Typography>
             </Box>
-            <Box onClick={(e) =>onClick(e)} sx={{width:60,}}>
+            <Box id="profile" onClick={(e) =>onClick(e)} sx={{width:60,}}>
                 <AccountBoxOutlinedIcon sx={{color:profile,marginLeft:3}}  fontSize="large"/>                  
                 <Typography sx={{fontWeight:'bold', color:profile,position:'absolute',bottom:buttom,right:21}}>
                     {profile == "#ef507b" ? "프로필" : ""}
