19e03978f99a043af360fc2d49f6e83cc31efc4e
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-11-30 15:43:24 +0900

    Update graph.ts

--- src/data/graph.ts
+++ src/data/graph.ts
@@ -1,7 +1,7 @@
 import { getGraphBpmHrvArr } from "../axios/api/serverApi";
 import { graphBpmHrvArr } from "../axios/interface/graph";
 
-export const getGraphBpmHrvArrData = async (eq:string,time:string[]):Promise<graphBpmHrvArr[]> => {   
-        const data:graphBpmHrvArr[] = await getGraphBpmHrvArr(`/mslbpm/webGraphBpmHrvArr?eq=${eq}&startDate=${time[0]} 23:59:59&endDate=${time[1]}`)   
+export const getGraphBpmHrvArrData = async (eq:string,nowTime:string,time:string[]):Promise<graphBpmHrvArr[]> => {   
+        const data:graphBpmHrvArr[] = await getGraphBpmHrvArr(`/mslbpm/webGraphBpmHrvArr?eq=${eq}&startDate=${nowTime}&endDate=${time[1]}`)   
         return data;   
  }  
