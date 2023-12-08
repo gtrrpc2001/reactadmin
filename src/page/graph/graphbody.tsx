7eafd6b8d6a28ba163e10527967ea5379ee6fa08
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-12-08 16:53:01 +0900

    Update graphbody.tsx

--- src/page/graph/graphbody.tsx
+++ src/page/graph/graphbody.tsx
@@ -26,7 +26,11 @@ export const GraphBody = ({names,marginTop}:Props) => {
 
     useEffect(()=>{
         setList(names)
-    },[names])    
+    },[names])
+
+    const getCheckMaxValue = (value:number):number => {
+        return (value > 180) ? 180 : value
+    }
 
     async function getData(id:string,time:string,kindButton:graphKindButton,setData:React.Dispatch<React.SetStateAction<any[]>>)
     {
@@ -50,7 +54,7 @@ export const GraphBody = ({names,marginTop}:Props) => {
                     default : 
                         result = await getGraphBpmHrvArrData(id,time,calTime)
                         v = result?.map((d)=>{
-                             return {bpm:d.bpm,hrv:d.hrv,arr:d.count,time:getWritetimeSelectHour_Min(d.writetime)}
+                             return {bpm:getCheckMaxValue(d.bpm),hrv:getCheckMaxValue(d.hrv),arr:d.count,writetime:getWritetimeSelectHour_Min(d.writetime)}
                          })
                         break;
 
