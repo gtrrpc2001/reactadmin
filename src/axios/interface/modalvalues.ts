9e4d9d79cfda6e7beff09e59e97562b207a8a454
gtrrpc2001 <cbdgjs3548@naver.com>
2024-07-22 16:33:20 +0900

    interface

--- src/axios/interface/modalvalues.ts
+++ src/axios/interface/modalvalues.ts
@@ -1,9 +1,14 @@
-export interface modalValues{
-    writetime:string
-    bpm:number
-    arrCnt:number
-    actCal:number
-    step:number
-    temp:number
-    distance:number
+export interface modalValues {
+  writetime: string;
+  bpm: number;
+  arrCnt: number;
+  actCal: number;
+  step: number;
+  temp: number;
+  distance: number;
+}
+
+export interface ModalDefaultType {
+  open: boolean;
+  setModalOpen: (value: React.SetStateAction<boolean>) => void;
 }
