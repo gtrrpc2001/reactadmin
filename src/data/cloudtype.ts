5937d33a5c3b2abfe6c725190377df8ca63a7c39
gtrrpc2001 <cbdgjs3548@gmail.com>
2025-01-09 10:36:33 +0900

    test done

--- src/data/cloudtype.ts
+++ src/data/cloudtype.ts
@@ -1,17 +1,16 @@
 import { postData } from "../axios/api/serverApi";
 import { CloudTypeData } from "../axios/interface/cloudtype";
-import { decrypt, encrypt } from "../func/crypto";
 
 export const getCloudTypeData = async (): Promise<CloudTypeData[]> => {
-    const data = await postData<string>('/CloudType/stat');
-    const decryptData = decrypt<CloudTypeData[]>(data);
-    return decryptData;
+    const data = await postData<CloudTypeData[]>('/CloudType/stat');
+    // const decryptData = decrypt<CloudTypeData[]>(data);
+    return data;
 };
 
 export const setCloudTypeStart = async (body: any): Promise<void> => {
-    await postData<void>('/CloudType/start', { data: encrypt(body) });
+    await postData<void>('/CloudType/start', body);
 };
 
 export const setCloudTypeStop = async (body: any): Promise<void> => {
-    await postData<void>('/CloudType/stop', { data: encrypt(body) });
+    await postData<void>('/CloudType/stop', body);
 };
